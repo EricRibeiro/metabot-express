adcc859a809abd33ff5f6da4efd779f516edc1d0
RonanFelipe <ronan.nascimento@sga.pucminas.br>
2021-04-13 01:42:10 -0300

    set headers

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ app.use(express.json());
 
 app.use(function (req, res, next){
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 })
 
