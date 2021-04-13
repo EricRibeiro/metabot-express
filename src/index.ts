4f7234993df6e154b31cfb019e95e2ebe16f6e95
RonanFelipe <ronan.nascimento@sga.pucminas.br>
2021-04-13 01:33:10 -0300

    add headers

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,11 @@ dotenv.config();
 const app = express()
 app.use(express.json());
 
+app.use(function (req, res, next){
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+})
+
 const port = process.env.PORT || 3000
 
 app.post('/events', async (req, res) => {
